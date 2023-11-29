f301b885273ac0b22ce9b4a2c52f5eb78f6ea241
Alexandr (SeriousMistake) <sanekz01@gmail.com>
2023-11-29 16:09:42 +0500

    Добавлена обработка сбора обратной связи от пользователя

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 
 def index(request):
     return render(request, 'catalog/index.html')
 
 
 def contacts(request):
+    if request.method == 'POST':
+
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
+            message = form.cleaned_data['message']
+            print(f'Name: {name}, Phone: {phone}, Message: {message}')
+    else:
+        form = ContactForm()
     return render(request, 'catalog/contacts.html')
