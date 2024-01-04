29c8f5dff6d07336b48133abdb8aff2e19b637b5
Serious (Alexander) <sanekzh01@gmail.com>
2024-01-04 06:50:12 +0500

    add homework 22.2

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 NULLABLE = {'blank': True, 'null': True}
 
 
 class Product(models.Model):
+    """
+    Модель для продуктов
+    """
     product_name = models.CharField(max_length=150, verbose_name="наименование")
     description = models.TextField(max_length=400, verbose_name="описание")
     image = models.ImageField(upload_to='catalog/product_image/', verbose_name="изображение", **NULLABLE)
@@ -11,11 +15,18 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='цена за покупку')
     date_of_add = models.DateField(auto_now=False, auto_now_add=True, verbose_name='дата создания')
     last_modified = models.DateField(auto_now=True, auto_now_add=False, verbose_name='дата изменения')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Продавец')
 
     def __str__(self):
+        """
+        Возвращение строкового представления объекта
+        """
         return f'{self.product_name} {self.price} {self.category}'
 
     class Meta:
+        """
+        Настройки для наименования объекта/объектов
+        """
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
         ordering = ('product_name',)
@@ -46,18 +57,30 @@ class Version(models.Model):
 
 
 class Category(models.Model):
+    """
+    Модель для категории
+    """
     category_name = models.CharField(max_length=150, verbose_name='категория')
     description = models.CharField(max_length=400, verbose_name='описание', **NULLABLE)
 
     def __str__(self):
+        """
+        Возвращение строкового представления объекта
+        """
         return f'{self.category_name}'
 
     class Meta:
+        """
+        Настройки для наименования объекта/объектов
+        """
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
 
 
 class Blog(models.Model):
+    """
+    Модель для блога
+    """
     title = models.CharField(max_length=150, verbose_name='Заголовок')
     slug = models.CharField(max_length=150, **NULLABLE, verbose_name='slug')
     content = models.TextField(**NULLABLE, verbose_name='Содержимое')
@@ -67,9 +90,15 @@ class Blog(models.Model):
     views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
 
     def __str__(self):
+        """
+        Возвращение строкового представления объекта
+        """
         return f'Блог: {self.title}'
 
     class Meta:
+        """
+        Настройки для наименования объекта/объектов
+        """
         verbose_name = 'Блог'
         verbose_name_plural = 'Блоги'
         ordering = ('title',)
